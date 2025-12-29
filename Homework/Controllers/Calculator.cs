e289e461997b4c39b11c798992acb70c18eb369a
Art'm <kasko.artem45@gmail.com>
2025-12-29 20:38:49 +0300

    added endpoints to the calculator controller (plus, minus, multiplication, division)

--- Homework/Controllers/Calculator.cs
+++ Homework/Controllers/Calculator.cs
@@ -6,5 +6,31 @@ namespace Homework.Controllers;
 [Route("api/[controller]")]
 public class CalculatorController : Controller
 {
+    [HttpGet("post")]
+    public double Plus([FromQuery] double x, [FromQuery] double y)
+    {
+        HttpContext.Response.StatusCode = 200;
+        return x + y;
+    }
     
+    [HttpGet("minus")]
+    public double Minus([FromQuery] double x, [FromQuery] double y)
+    {
+        HttpContext.Response.StatusCode = 200;
+        return x - y;
+    }
+    
+    [HttpGet("multiply/{x}/{y}")]
+    public double Multiply([FromRoute] double x, [FromRoute] double y)
+    {
+        HttpContext.Response.StatusCode = 300;
+        return x * y;
+    }
+
+    [HttpGet("divide/{x}/{y}")]
+    public double Divide([FromRoute] double x, [FromRoute] double y)
+    {
+        HttpContext.Response.StatusCode = 300;
+        return x / y;
+    }
 }
